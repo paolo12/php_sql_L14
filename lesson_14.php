ce8a8028741a616233d5352cc90bb44d64c07135
pavel <ppyakov@gmail.com>
2017-11-06 18:28:08 +0300

    next step

--- lesson_14.php
+++ lesson_14.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=global', 'root', 'qwerty');
+$pdo = new PDO('mysql:host=localhost;dbname=global;charset=UTF8', 'root', 'qwerty');
 date_default_timezone_set('Europe/Moscow');
 $time = date("Y-m-d H:m:s");
 
